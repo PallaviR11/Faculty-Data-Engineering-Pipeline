c8196c86738570640922c958e95492b06e9111f9
Pallavi <pallavivanshi@gmail.com>
2026-01-18 17:55:57 +0530

    stop tracking unnecessary files

--- Extra/a.py
+++
@@ -1,7 +0,0 @@
-print('------------------')
-print("1:", __name__)
-def main():
-    print("Running scraper...")
-print(2)
-print('------------------')
-
--- Extra/b.py
+++
@@ -1,6 +0,0 @@
-print('++++++++++++')
-import a
-import importlib
-importlib.reload(a)  # force a.py to execute
-print("Runner __name__:", __name__)
-print('++++++++++++')
