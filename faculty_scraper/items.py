17d8aeb4be28535cf273d806bf4e06e64d5d2127
Pallavi Raghuvanshi <137675291+PallaviR11@users.noreply.github.com>
2026-01-23 23:22:22 +0530

    correct items.py

--- faculty_scraper/items.py
+++ faculty_scraper/items.py
@@ -1,16 +1,15 @@
 import scrapy
 
 class FacultyItem(scrapy.Item):
-    image_url = scrapy.Field()
+    faculty_type = scrapy.Field()
     name = scrapy.Field()
+    professional_link = scrapy.Field()
     qualification = scrapy.Field()
     phone = scrapy.Field()
-    address = scrapy.Field()
     email = scrapy.Field()
-    professional_link = scrapy.Field()
+    address = scrapy.Field()
     biography = scrapy.Field()
     specialization = scrapy.Field()
     publications = scrapy.Field()
     teaching = scrapy.Field()
     research = scrapy.Field()
-    content_hash = scrapy.Field()
